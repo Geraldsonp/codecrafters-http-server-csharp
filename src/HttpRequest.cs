631b8d053817a0039b00966093e82275d062c5e2
Geraldsonp <geraldson.a.p@gmail.com>
2024-04-24 19:54:10 -0400

    completes reading request line

---
+++ src/HttpRequest.cs
@@ -0,0 +1,41 @@
+﻿namespace codecrafters_http_server;
+
+public class HttpRequest
+{
+    public string Path { get; set; }
+    public string Method { get; set; }
+    public string Body { get; set; }
+    public Dictionary<string, string> Headers { get; set; }
+
+    public HttpRequest(string method, string path, string body, Dictionary<string, string> headers)
+    {
+        Method = method;
+        Path = path;
+        Body = body;
+        Headers = headers;
+    }
+
+    public static HttpRequest Parse(string request)
+    {
+        var lines = request.Split("\r\n");
+        var firstLine = lines[0].Split(" ");
+        var method = firstLine[0];
+        var path = firstLine[1];
+        var headers = new Dictionary<string, string>();
+        var body = "";
+        var bodyIndex = Array.IndexOf(lines, "") + 1;
+        if (bodyIndex < lines.Length)
+        {
+            body = lines[bodyIndex];
+        }
+
+        for (var i = 1; i < bodyIndex; i++)
+        {
+            var header = lines[i].Split(": ");
+            if (header.Length == 2)
+                headers.Add(header[0], header[1]);
+        }
+
+        return new HttpRequest(method, path, body, headers);
+    }
+}
--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using codecrafters_http_server;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -21,9 +22,33 @@ try
         Console.WriteLine("Connected!");
         var stream = client.GetStream();
 
-        var response = Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
-        await stream.WriteAsync(response);
-        Console.WriteLine("Sent: {0}", response.Length);
+        var httpRequest = await ReadIncoming(stream);
+
+        if (httpRequest.Path == "/")
+        {
+            await WriteResponse(stream, "HTTP/1.1 200 OK\r\n\r\n");
+        }
+        else
+        {
+            await WriteResponse(stream, "HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+    }
+
+    async Task WriteResponse(NetworkStream stream, string responseText)
+    {
+        var response = Encoding.ASCII.GetBytes(responseText);
+        await stream.WriteAsync(response, 0, response.Length);
+        Console.WriteLine("Sent: {0}", responseText);
+    }
+
+    async Task<HttpRequest> ReadIncoming(Stream stream)
+    {
+        var buffer = new byte[1024];
+        var bytesRead = await stream.ReadAsync(buffer);
+        var data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Received: {0}", data);
+
+        return HttpRequest.Parse(data);
     }
 }
 catch (Exception e)
